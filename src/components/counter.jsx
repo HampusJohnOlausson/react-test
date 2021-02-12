cab8fc4cfb2282e252f80dc85a3caf43f93e5561
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-12 10:56:44 +0100

    map

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,26 +2,31 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0
+        count: 0,
+        tags: ["tag1", "tag2", "tag3"]
      };
 
-     styles = {
-        fontWeight: 50,
-        fontWeight: 'bold'
-     };
 
     render() { 
+
         return (
         <div>
-            <span style={this.styles} className="badge badge-primary">{this.formatCount()}</span>
-            <button>Increment</button>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className= "btn btn-secondary btn-sm">Increment</button>
+            <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
         </div>
         );
     }
 
-    formatCount(){
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
         const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        return count === 0 ? 'zero' : count;
     }
 }
  
