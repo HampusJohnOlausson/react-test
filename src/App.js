89ec06fad0dbe7c63304a4bdcd3f76c1194ee060
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-20 11:45:49 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import React, { Component } from 'react'
-
+import Greet from './components/Greet';
 
 export default class App extends Component {
 
   state = {
-    count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
-  };
 
-  handlePress = () => {
-    this.setState({ count: this.state.count + 1});
-  }
+  };
 
 
   render() {
@@ -19,13 +14,8 @@ export default class App extends Component {
 
     return (
       <>
-      <ul>
-        {this.state.tags.map(tag => 
-          <li key={tag}>{tag}</li>
-        )}
-      </ul>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handlePress}  >Click</button>
+      <Greet name="Bruce"/>
+      <Greet name="Diana"/>
       </>
     )
   }
