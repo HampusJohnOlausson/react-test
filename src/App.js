0b65c3f9c0a3a10c2876175b049de1bac5ba4be3
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-22 20:17:57 +0100

    #

--- src/App.js
+++ src/App.js
@@ -1,49 +1,13 @@
 import './App.css';
 import React, { Component } from 'react';
-import Form from './components/Form';
-import List from './components/List';
 
 
 export default class App extends Component {
-  state = {
-    input: "",
-    todos: [
-      { value: "clean kictchen", done: false },
-      { value: "buy stuff", done: true },
-    ],
-  };
-
-  handleChange = (event) => {
-    this.setState({ input: event.target.value });
-  };
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    const newTodo = {
-      value: this.state.input,
-      done: false
-    };
-    const todos = this.state.todos;
-    todos.push(newTodo);
-    this.setState({ todos: todos, input: '' })
-  }
-
-  handleClick = (index) => {
-    const todos = this.state.todos;
-    todos[index].done = !todos[index].done;
-    this.setState({ todos });
-  }
-
+  
   render() {
     return (
       <>
-        <Form 
-        handleChange={this.handleChange} 
-        input={this.state.input}
-        handleSubmit={this.handleSubmit} />
-        <List
-        todos={this.state.todos}
-        handleClick={this.handleClick} />
+      
       </>
     );
   }
