4ba5efb93d7857a3e2f763d0e921ea4a87eccbfb
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-23 16:50:24 +0100

    react Router test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
 import React, { Component } from 'react';
-import MovieList from './components/MovieList';
-import Nav from './components/Nav';
-import {MovieContext} from './components/MovieContext';
-
 
 export default class App extends Component {
   
@@ -11,12 +7,10 @@ export default class App extends Component {
   render() {
 
     return (
-      
-      <MovieContext>
-      <Nav/>
-        <MovieList/>
-      </MovieContext>
-    );
+      <React.Fragment>
+        
+      </React.Fragment>
+    )
   }
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 const Nav = () => {
     return (
         <div>
-            <h3>Dev ed</h3>
-            <p>list of movies</p>
+            <ul>
+                <li>About</li>
+                <li>Shop</li>
+            </ul>
         </div>
     )
 }
 
-export default Nav;
+export default Nav
