c0e597a5e84c1f29f38641baaf464f9e35d5ce3d
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-13 22:23:38 +0100

    simple quiz app setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
 import Layout from './components/Layout'
-import { MovieProvider } from './components/MovieListContext'
 
 
 export default class App extends Component {
   render() {
     return (
-      <MovieProvider>
+      <>
         <Layout/>
-      </MovieProvider>
+      </>
     )
   }
 }
